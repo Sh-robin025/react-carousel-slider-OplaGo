ad07c9096fb5ccb81112efefcc22c780a9f71f35
Shahadat Robin <sh.robin025@gmail.com>
2021-08-18 16:38:12 +0600

    setup done

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Item from "./Item";
+
+const Carousel = () => {
+  return (
+    <div>
+      <Item />
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ src/components/carousel/Item.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Item = () => {
+  return (
+    <div>
+      <h5>here is carousel item card</h5>
+    </div>
+  );
+};
+
+export default Item;
